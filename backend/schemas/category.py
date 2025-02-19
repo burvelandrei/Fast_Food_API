43e24bceaa98b1ded6a5d251655941a5c0278701
Andrei Burvel <burvelandrei@gmail.com>
2025-02-19 13:13:05 +0300

    V 1.15 добавлено поле products в схему category

--- backend/schemas/category.py
+++ backend/schemas/category.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel
+from typing import List
+from schemas.product import ProductOut
 
 
 class CategoryOut(BaseModel):
     id: int
     name: str
+    products: List[ProductOut]
 
     class Config:
         from_attributes = True
