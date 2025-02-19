4b1e3780e254fdb6a24b546a3bdcedac379d5bb9
Andrei Burvel <burvelandrei@gmail.com>
2025-02-19 19:11:35 +0300

    V 1.22 добавлен запрет доступа без api key в роутер category

--- backend/routers/category.py
+++ backend/routers/category.py
@@ -3,12 +3,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from db.connect import get_session
 from schemas.category import CategoryOut
 from db.operations import CategoryDO
+from dependencies import verify_api_key
 
 
 router = APIRouter(prefix="/category", tags=["Category"])
 
 
 @router.get("/", response_model=list[CategoryOut])
-async def get_category(session: AsyncSession = Depends(get_session)):
+async def get_category(
+    session: AsyncSession = Depends(get_session),
+    api_key: str = Depends(verify_api_key),
+):
     category = await CategoryDO.get_all(session=session)
     return category
