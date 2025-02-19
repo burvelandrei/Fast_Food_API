92643de1241d903d325f0d012169301dd666ee9b
Andrei Burvel <burvelandrei@gmail.com>
2025-02-19 20:50:08 +0300

    V 1.25 добавленo подключение к redis

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,3 +1,4 @@
+from redis.asyncio import Redis
 from fastapi import Depends, HTTPException
 from fastapi.security import APIKeyHeader
 from environs import Env
@@ -9,12 +10,15 @@ env = Env()
 env.read_env()
 
 
-API_KEY_BOT = env("API_KEY_BOT")
-
 api_key_header = APIKeyHeader(name="X-API-Key")
 
 
 async def verify_api_key(api_key: str = Depends(api_key_header)):
-    if api_key != API_KEY_BOT:
+    if api_key != env("API_KEY_BOT"):
         raise HTTPException(status_code=403, detail="Invalid API key")
     return api_key
+
+
+async def get_redis():
+    redis = Redis(host=env("REDIS_HOST"), port=env("REDIS_PORT"), decode_responses=True)
+    return redis
