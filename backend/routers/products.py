91173844d20a3878da859b546b5a64692f5e7fbf
Andrei Burvel <burvelandrei@gmail.com>
2025-02-19 19:09:58 +0300

    V 1.21 добавлен запрет доступа без api key

--- backend/routers/products.py
+++ backend/routers/products.py
@@ -3,19 +3,28 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from db.connect import get_session
 from schemas.product import ProductOut
 from db.operations import ProductDO
+from dependencies import verify_api_key
 
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
 
 @router.get("/", response_model=list[ProductOut])
-async def get_products(category_id: int = Query(None), session: AsyncSession = Depends(get_session)):
-    products = await ProductDO.get_all(category_id = category_id, session=session)
+async def get_products(
+    category_id: int = Query(None),
+    session: AsyncSession = Depends(get_session),
+    api_key: str = Depends(verify_api_key),
+):
+    products = await ProductDO.get_all(category_id=category_id, session=session)
     return products
 
 
 @router.get("/{product_id}", response_model=ProductOut)
-async def get_product(product_id: int, session: AsyncSession = Depends(get_session)):
+async def get_product(
+    product_id: int,
+    session: AsyncSession = Depends(get_session),
+    api_key: str = Depends(verify_api_key),
+):
     product = await ProductDO.get_by_id(id=product_id, session=session)
     if product is None:
         raise HTTPException(status_code=404, detail="Product not found")
