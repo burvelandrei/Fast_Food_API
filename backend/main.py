4c96e99a65b0c4698f4b10d95b64680a5bb02b7d
Andrei Burvel <burvelandrei@gmail.com>
2025-02-20 14:19:49 +0300

    V 1.32 добавлен router для подтверждения заказа и получения всех заказов юзера

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import products, category, users, cart
+from routers import products, category, users, carts, orders
 
 
 app = FastAPI(title="FastFood API")
 app.include_router(products.router)
 app.include_router(category.router)
 app.include_router(users.router)
-app.include_router(cart.router)
+app.include_router(carts.router)
+app.include_router(orders.router)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ backend/routers/orders.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.connect import get_session
+from schemas.order import OrderOut
+from db.operations import OrderDO, OrderItemDO
+from dependencies import verify_api_key, get_redis
+from services.redis_cart import get_cart, remove_cart
+
+
+router = APIRouter(prefix="/orders", tags=["Orders"])
+
+
+@router.post("/{user_id}")
+async def confirmation_order(
+    user_id: int,
+    redis=Depends(get_redis),
+    session: AsyncSession = Depends(get_session),
+    api_key: str = Depends(verify_api_key),
+):
+    cart = await get_cart(user_id, redis)
+    await OrderDO.add(user_id=user_id, session=session, values=cart)
+    await remove_cart(user_id, redis)
+    return {"message": "Order add"}
+
+
+@router.get("/{user_id}", response_model=list[OrderOut])
+async def get_all_order(
+    user_id: int,
+    session: AsyncSession = Depends(get_session),
+    api_key: str = Depends(verify_api_key),
+):
+    orders = await OrderDO.get_all(user_id, session)
+    return orders
