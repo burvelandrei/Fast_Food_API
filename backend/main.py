c5905a47d37c5c7fbd05188a16e8d4c95ceb4852
Andrei Burvel <burvelandrei@gmail.com>
2025-02-20 10:57:03 +0300

    V 1.27 добавлен route cart

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import products, category, users
+from routers import products, category, users, cart
 
 
 app = FastAPI(title="FastFood API")
 app.include_router(products.router)
 app.include_router(category.router)
 app.include_router(users.router)
+app.include_router(cart.router)
 
 
 
