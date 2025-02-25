002bc58cf0f525f4c3777e2e4ebe2093a8ee152e
Andrei Burvel <burvelandrei@gmail.com>
2025-02-25 13:15:14 +0300

    V 1.44 убран x-api-key т.к. используется jwt

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -10,15 +10,6 @@ env = Env()
 env.read_env()
 
 
-api_key_header = APIKeyHeader(name="X-API-Key")
-
-
-async def verify_api_key(api_key: str = Depends(api_key_header)):
-    if api_key != env("API_KEY_BOT"):
-        raise HTTPException(status_code=403, detail="Invalid API key")
-    return api_key
-
-
 async def get_redis():
     redis = Redis(host=env("REDIS_HOST"), port=env("REDIS_PORT"), decode_responses=True)
     return redis
