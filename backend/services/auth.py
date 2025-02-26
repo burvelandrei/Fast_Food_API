7c7aab6ba58cb1f85131a59fff4a91f2e2609f5b
Andrei Burvel <burvelandrei@gmail.com>
2025-02-26 12:56:53 +0300

    V 1.52 изменена функция get_current_user (добавлен фозврфт пользователя по jwt из тг бота)

--- backend/services/auth.py
+++ backend/services/auth.py
@@ -3,12 +3,12 @@ from datetime import datetime, timedelta
 from typing import Annotated
 from fastapi import HTTPException, Depends, status
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 from environs import Env
 from jwt.exceptions import InvalidTokenError
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.connect import get_session
-from schemas.token import TokenData, Token
+from schemas.token import TokenData
 from db.operations import UserDO
 
 
@@ -17,6 +17,7 @@ env.read_env()
 
 
 SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY_BOT = env("SECRET_KEY_BOT")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 REFRESH_TOKEN_EXPIRE_DAYS = 7
@@ -56,6 +57,7 @@ def create_access_token(data: dict):
 def create_refresh_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -68,17 +70,35 @@ async def get_current_user(
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
     )
+    expired_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token expired",
+    )
+    payload = None
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email = payload.get("sub")
-        if email is None:
+    except jwt.ExpiredSignatureError:
+        raise expired_exception
+    except jwt.PyJWTError:
+        pass
+
+    if not payload:
+        try:
+            payload = jwt.decode(token, SECRET_KEY_BOT, algorithms=[ALGORITHM])
+        except jwt.ExpiredSignatureError:
+            raise expired_exception
+        except jwt.PyJWTError:
             raise credentials_exception
-        token_data = TokenData(email=email)
-    except InvalidTokenError:
+    email = payload.get("email")
+    tg_id = payload.get("tg_id")
+    if not email and not tg_id:
         raise credentials_exception
-    user = await UserDO.get_by_email(email=token_data.email, session=session)
+    token_data = TokenData(email=email, tg_id=tg_id)
+    if token_data.email:
+        user = await UserDO.get_by_email(email=token_data.email, session=session)
+    else:
+        user = await UserDO.get_by_tg_id(tg_id=token_data.tg_id, session=session)
     if user is None:
         raise credentials_exception
     return user
