31aeb6b198c1eb2bda4a47c704555e9c6f6f1196
Andrei Burvel <burvelandrei@gmail.com>
2025-02-26 13:47:59 +0300

    V 1.57 прописан Depends для получения юзера по jwt

--- backend/routers/carts.py
+++ backend/routers/carts.py
@@ -18,26 +18,27 @@ async def add_item_to_cart(
     return await add_to_cart(user.id, item, redis)
 
 
-@router.get("/{user_id}")
+@router.get("/")
 async def get_cart_user(
-    user_id: int, redis=Depends(get_redis)
+    user: UserOut = Depends(get_current_user),
+    redis=Depends(get_redis),
 ):
-    return await get_cart(user_id, redis)
+    return await get_cart(user.id, redis)
 
 
-@router.get("/{user_id}/{product_id}")
+@router.get("/{product_id}")
 async def get_cart_item_user(
-    user_id: int,
     product_id: int,
+    user: UserOut = Depends(get_current_user),
     redis=Depends(get_redis),
 ):
-    return await get_cart_item(user_id, product_id, redis)
+    return await get_cart_item(user.id, product_id, redis)
 
 
-@router.delete("/{user_id}/{product_id}")
+@router.delete("/{product_id}")
 async def delete_item_from_cart(
-    user_id: int,
     product_id: int,
+    user: UserOut = Depends(get_current_user),
     redis=Depends(get_redis),
 ):
-    return await remove_item(user_id, product_id, redis)
+    return await remove_item(user.id, product_id, redis)
--- backend/routers/orders.py
+++ backend/routers/orders.py
@@ -2,32 +2,34 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.connect import get_session
 from schemas.order import OrderOut
+from schemas.user import UserOut
 from db.operations import OrderDO, OrderItemDO
 from dependencies import get_redis
 from services.redis_cart import get_cart, remove_cart
+from services.auth import get_current_user
 
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
 
-@router.post("/{user_id}")
+@router.post("/")
 async def confirmation_order(
-    user_id: int,
+    user: UserOut = Depends(get_current_user),
     redis=Depends(get_redis),
     session: AsyncSession = Depends(get_session),
 ):
-    cart = await get_cart(user_id, redis)
+    cart = await get_cart(user.id, redis)
     if cart:
-        await OrderDO.add(user_id=user_id, session=session, values=cart)
-        await remove_cart(user_id, redis)
+        await OrderDO.add(user_id=user.id, session=session, values=cart)
+        await remove_cart(user.id, redis)
         return {"message": "Order add"}
     return {"message": "No products in cart"}
 
 
-@router.get("/{user_id}", response_model=list[OrderOut])
+@router.get("/", response_model=list[OrderOut])
 async def get_all_order(
-    user_id: int,
+    user: UserOut = Depends(get_current_user),
     session: AsyncSession = Depends(get_session),
 ):
-    orders = await OrderDO.get_all(user_id, session)
+    orders = await OrderDO.get_all(user.id, session)
     return orders
