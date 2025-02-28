41051d4523cd90c3c228cc8591de794e01ca4f94
Andrei Burvel <burvelandrei@gmail.com>
2025-02-28 11:59:18 +0300

    V 1.67 добвалены схемы cart и cartitem для вывода json

--- backend/schemas/cart.py
+++ backend/schemas/cart.py
@@ -1,6 +1,20 @@
+from typing import List
 from pydantic import BaseModel, Field
+from schemas.product import ProductOut
+
+
+class CartItemOut(BaseModel):
+    product: ProductOut
+    quantity: int
+    total_price: float
+
+
+class CartOut(BaseModel):
+    user_id: int
+    cart_items: List[CartItemOut] = []
+    total_amount: float
 
 
 class CartItemCreate(BaseModel):
     product_id: int
-    quantity: int = Field(ge=0)
+    quantity: int = Field(ge=1, description="Quantity must be at least 1")
