0784414263c698f73c7cffc136568e7931c1d18c
Andrei Burvel <burvelandrei@gmail.com>
2025-03-07 17:22:04 +0300

    V 2.10 добавлено поле photo_name, переработано поле photo_url (возвращает фото только если есть в s3 иначе None)

--- schemas/product.py
+++ schemas/product.py
@@ -1,6 +1,7 @@
 import os
 from decimal import Decimal
 from pydantic import BaseModel, Field, computed_field
+from services.s3_utils import check_file_exists
 
 
 STATIC_DIR = "static/products/"
@@ -12,19 +13,18 @@ class ProductOut(BaseModel):
     description: str | None
     price: float
     discount: int
+    photo_name: str
 
-    # Поле для формирования пути на фото продукта
+    # Поле для формирования пути на фото продукта (если оно есть в s3 иначе None)
     @computed_field
-    def image_url(self) -> str | None:
-        image_path = f"{STATIC_DIR}product_{self.id}.jpg"
-        return (
-            f"{STATIC_DIR}product_{self.id}.jpg" if os.path.exists(image_path) else None
-        )
+    def photo_url(self) -> str | None:
+        photo_path = f"{STATIC_DIR}{self.photo_name}"
+        return f"/{photo_path}" if check_file_exists(key=photo_path) else None
 
     # Поле для вычисления финальной суммы (сумма * скидку)
     @computed_field
     def final_price(self) -> Decimal:
-        return self.price - (self.price*(self.discount/100))
+        return self.price - (self.price * (self.discount / 100))
 
 
 class ProductCreate(BaseModel):
