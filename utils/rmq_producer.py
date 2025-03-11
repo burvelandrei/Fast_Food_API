63f516b3072b32aa1278d7457fcbd963f743c8ae
Andrei Burvel <burvelandrei@gmail.com>
2025-03-11 15:40:50 +0300

    V 2.23 добавлена утилита для отправки в rabbitmq информации об успешном подтверждении почты

---
+++ utils/rmq_producer.py
@@ -0,0 +1,20 @@
+import aio_pika
+import json
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+
+# Функция для публикации в rabbitmq информации об успешном подтверждении почты
+async def publish_email_to_broker(event_data: dict):
+    connection = await aio_pika.connect_robust(
+        f"amqp://{env('RMQ_USER')}:{env('RMQ_PASSWORD')}@localhost/"
+    )
+    channel = await connection.channel()
+    await channel.default_exchange.publish(
+        aio_pika.Message(body=json.dumps(event_data).encode()),
+        routing_key="user_confirmations",
+    )
+    await connection.close()
