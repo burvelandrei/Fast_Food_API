488988140943d1a0893133323c45d5a02dc5f962
Andrei Burvel <burvelandrei@gmail.com>
2025-03-14 01:03:36 +0300

    V 2.40 логически перенесена схема ProductCartOut из product в cart

--- schemas/cart.py
+++ schemas/cart.py
@@ -1,7 +1,34 @@
 from typing import List
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
 from pydantic import BaseModel, Field, computed_field
-from schemas.product import ProductCartOut
+from utils.s3_utils import check_file_exists
+
+
+STATIC_DIR = "static/products/"
+
+
+class ProductCartOut(BaseModel):
+    id: int
+    name: str
+    description: str | None
+    photo_name: str
+    size_id: int
+    size_name: str
+    price: Decimal
+    discount: int
+
+    # Поле для вычисления финальной суммы (сумма * скидку)
+    @computed_field
+    def final_price(self) -> Decimal:
+        discount_decimal = Decimal(self.discount) / Decimal(100)
+        final_price = self.price - (self.price * discount_decimal)
+        return final_price.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+
+    @computed_field
+    def photo_url(self) -> str | None:
+        """Формирование URL для фото"""
+        photo_path = f"{STATIC_DIR}{self.photo_name}"
+        return f"/{photo_path}" if check_file_exists(key=photo_path) else None
 
 
 class CartItemOut(BaseModel):
